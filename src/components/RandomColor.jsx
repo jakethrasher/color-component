384d90d2d318ed19340f17d782aa8babe6dc47b9
Jake Thrasher <mjakethrasher@gmail.com>
2021-05-11 10:16:43 -0700

    refactor

--- src/components/RandomColor.jsx
+++ src/components/RandomColor.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-export default function RandomColor({color}) {
-    return (
-        <div style={{height:'350px', width: '350px', background:color,border:'1px solid black'}}>
+export default function RandomColor({babyPicture,color}) {
+
+    if(babyPicture){
+        return (<img src='https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg' style={{height:'350px', width:'350px'}}/>)
+    }
+    else return (
+        <div style={{height:'350px', width: '350px',        background:color,border:'1px solid black'}}>
         </div>
     )
 }
--- src/containers/ColorContainer.jsx
+++ src/containers/ColorContainer.jsx
@@ -12,28 +12,21 @@ export default class ColorContainer extends Component {
             const color = getColor();
             
             if(color === this.state.bgColor){
-                console.log(color, 'same color')
                 this.setState({babyPicture:true})
             }else{
                 this.setState({
                     bgColor:color,
                     babyPicture:false,
                 })
-                console.log(color)
             }
         },500)
     }
     render() {
         return (
             <div>
-            {this.state.babyPicture?
-            <img src='https://i.dailymail.co.uk/i/pix/2016/03/18/15/324D202500000578-3498922-image-a-33_1458315465874.jpg' style={{height:'350px', width:'350px'}}/>
-            :
-            <div>
-                <RandomColor color={this.state.bgColor}/>
-            </div>}
+                <RandomColor babyPicture={this.state.babyPicture}color={this.state.bgColor}/>
             </div>
         )
     }
 }   
-    
+            
